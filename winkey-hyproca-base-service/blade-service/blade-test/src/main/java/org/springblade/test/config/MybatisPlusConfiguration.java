ca6db637416162fdbb5836ec5daacb13bdaf7357
BigXiaoan <41133812+BigXiaoan@users.noreply.github.com>
2021-05-20 11:39:09 +0800

    Delete winkey-hyproca-base-service directory

--- winkey-hyproca-base-service/blade-service/blade-test/src/main/java/org/springblade/test/config/MybatisPlusConfiguration.java
+++
@@ -1,9 +0,0 @@
-package org.springblade.test.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan("org.springblade.**.mapper.**")
-public class MybatisPlusConfiguration {
-}
--- winkey-hyproca-base-service/blade-service/blade-test/src/main/java/org/springblade/test/service/BlogService.java
+++
@@ -1,8 +0,0 @@
-package org.springblade.test.service;
-
-import org.springblade.core.mp.base.BaseService;
-import org.springblade.test.entity.Blog;
-
-
-public interface BlogService  extends BaseService<Blog> {
-}
